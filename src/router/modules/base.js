9733091a4d75269f6cc7f815e730dc914dc7529e
fengwenqi <1318756239@qq.com>
2020-05-22 17:15:16 +0800

    init

---
+++ src/router/modules/base.js
@@ -0,0 +1,17 @@
+const base = [{
+  path: '/',
+  redirect: '/home'
+}, {
+  path: '/home',
+  component: () => import('@/views/home'),
+  hidden: true
+}, {
+  path: '/login',
+  component: () => import('@/views/login/index'),
+  hidden: true
+}, {
+  path: '/404',
+  component: () => import('@/views/404'),
+  hidden: true
+}]
+export default base
---
+++ src/store/getters.js
@@ -0,0 +1,9 @@
+const getters = {
+  sidebar: state => state.app.sidebar,
+  device: state => state.app.device,
+  roles: state => state.user.roles,
+  userInfo: state => state.user.userInfo,
+  permission_routers: state => state.permission.routers,
+  addRouters: state => state.permission.addRouters
+}
+export default getters
